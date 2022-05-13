a5a4cacb5983aeb796211f9af0018d149a5259cf
Muhammad Naeem <m.naeem66622@outlook.com>
2022-05-13 16:59:38 +0500

    UI Improvements
    Gallery Modal Image  responsive on different screen sizes

--- js/modal_image.js
+++ js/modal_image.js
@@ -1,10 +1,11 @@
 // get images src onclick
 const images = document.querySelectorAll(".column img");
+let image;
 let imgSrc;
 images.forEach((img) => {
     img.addEventListener("click", (e) => {
         imgSrc = e.target.src;
-        console.log("We are on the way");
+        image = e.target;
 
         //run modal function
         imgModal(imgSrc);
@@ -21,6 +22,23 @@ let imgModal = (src) => {
     //adding image to modal
     const newImage = document.createElement("img");
     newImage.setAttribute("src", src);
+
+    //check the screen orientation and apply style to image
+    if (screen.availHeight > screen.availWidth) {
+        //for potrait screen
+        newImage.setAttribute("style", "width: 80%");
+    }
+    else {
+        //for landscape screen
+        if (image.naturalHeight > image.naturalWidth) {
+            //if picture potrait
+            newImage.setAttribute("style", "width: 55vh");
+        } else {
+            //if picture landscape
+            newImage.setAttribute("style", "width: 85vh");
+        }
+    }
+
     //creating the close button
     const closeBtn = document.createElement("i");
     closeBtn.setAttribute("class", "fas fa-times closeBtn");
